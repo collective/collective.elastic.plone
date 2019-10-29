5ddd7a6c059966810b8aeb78fd7cffc3a76c598f
Jens W. Klein <jk@kleinundpartner.at>
2019-10-29 18:17:40 +0100

    undo wrong logging

--- src/collective/es/plone/queueprocessor.py
+++ src/collective/es/plone/queueprocessor.py
@@ -20,16 +20,14 @@ class ElasticSearchIndexQueueProcessor(object):
     def index(self, obj, attributes=None):
         # get transaction id
         ts = TimeStamp(obj._p_serial)
-        logger.info(
-            index.delay("/".join(obj.getPhysicalPath()), ts.timeTime(), index_name())
-        )
+        index.delay("/".join(obj.getPhysicalPath()), ts.timeTime(), index_name())
 
     def reindex(self, obj, attributes=None, update_metadata=1):
         self.index(obj, attributes)
 
     def unindex(self, obj):
         uid = api.content.get_uuid(obj)
-        logger.info(unindex.delay(uid, index_name()))
+        unindex.delay(uid, index_name())
 
     def begin(self):
         pass
