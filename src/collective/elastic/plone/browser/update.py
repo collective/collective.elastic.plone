3b5c84d1f4af32d617a5503ae893e0e9c86331ae
Jens W. Klein <jk@kleinundpartner.at>
2019-11-19 18:38:01 +0100

    ignore portal_catalog

--- src/collective/elastic/plone/browser/update.py
+++ src/collective/elastic/plone/browser/update.py
@@ -6,11 +6,13 @@ from Products.Five.browser import BrowserView
 
 
 class UpdateElastisearch(BrowserView):
-
     def __call__(self):
         cat = api.portal.get_tool("portal_catalog")
         count = 0
         for path in cat._catalog.uids:
+            if path.endswith("/portal_catalog"):
+                # no idea why it is in the list, ignore
+                continue
             index.delay(path, 0, index_name())
             count += 1
         return "queued {0}".format(count)
