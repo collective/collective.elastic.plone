16a9ff1924e40102d315db4902ab570069db9b6a
Jens W. Klein <jk@kleinundpartner.at>
2019-11-19 18:29:12 +0100

    add simple reindex all in ES script

---
+++ src/collective/elastic/plone/browser/update.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from collective.elastic.ingest.celery import index
+from collective.elastic.plone.eslib import index_name
+from plone import api
+from Products.Five.browser import BrowserView
+
+
+class UpdateElastisearch(BrowserView):
+
+    def __call__(self):
+        cat = api.portal.get_tool("portal_catalog")
+        count = 0
+        for path in cat._catalog.uids:
+            index.delay(path, 0, index_name())
+            count += 1
+        return "queued {0}".format(count)
