957d85ac6acd0b922c88749a18c0d0805e47263e
ksuess <k.suess@rohberg.ch>
2022-07-14 14:05:29 +0200

    Remove invalid parameter 'use_ssl' for elasticsearch-py >= 8

--- src/collective/elastic/plone/eslib.py
+++ src/collective/elastic/plone/eslib.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from elasticsearch import Elasticsearch
+from collective.elastic.ingest import ELASTICSEARCH_7
 
 import logging
 import os
@@ -9,19 +10,20 @@ logger = logging.getLogger(__name__)
 
 
 def get_query_client():
-    """return elasticsearch client for.ingest
-    """
+    """return elasticsearch client for.ingest"""
     raw_addr = os.environ.get("ELASTICSEARCH_QUERY_SERVER", "http://localhost:9200")
     use_ssl = os.environ.get("ELASTICSEARCH_QUERY_USE_SSL", "0")
     use_ssl = bool(int(use_ssl))
     addresses = [x for x in raw_addr.split(",") if x.strip()]
     if not addresses:
         addresses.append("127.0.0.1:9200")
-    return Elasticsearch(
-        addresses,
-        use_ssl=use_ssl,
-        # here some more params need to be configured.
-    )
+    if ELASTICSEARCH_7:
+        return Elasticsearch(
+            addresses,
+            use_ssl=use_ssl,
+            # here some more params need to be configured.
+        )
+    return Elasticsearch(addresses)
 
 
 def index_name():
