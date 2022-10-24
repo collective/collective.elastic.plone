0735a93716f3c72a44781fc82d100f149c285392
Katja Süss <k.suess@rohberg.ch>
2022-10-24 06:16:27 +0200

    Fix imports for Plone 6

--- src/collective/elastic/plone/proxyindex.py
+++ src/collective/elastic/plone/proxyindex.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 from AccessControl import ClassSecurityInfo
-from AccessControl.class_init import InitializeClass
+
+try:
+    from AccessControl.class_init import InitializeClass
+except ImportError:
+    from App.class_init import InitializeClass
 from AccessControl.requestmethod import postonly
 from BTrees.IIBTree import IIBTree
 from collective.elastic.plone.eslib import get_query_client
@@ -11,7 +15,11 @@ from OFS.SimpleItem import SimpleItem
 from Products.GenericSetup.interfaces import ISetupEnviron
 from Products.GenericSetup.utils import NodeAdapterBase
 from Products.PageTemplates.PageTemplateFile import PageTemplateFile
-from Products.ZCatalog.query import IndexQuery
+
+try:
+    from Products.ZCatalog.query import IndexQuery
+except ImportError:
+    from Products.PluginIndexes.common.util import parseIndexRequest as IndexQuery
 from Products.PluginIndexes.interfaces import ISortIndex
 from zope.component import adapter
 from zope.interface import implementer
