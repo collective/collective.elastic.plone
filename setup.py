bbd28fd40823d060a23502781910256cfc8c0235
Jens W. Klein <jk@kleinundpartner.at>
2022-11-08 10:26:50 +0100

    Preparing release 1.0

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='collective.elastic.plone',
-    version='1.0a1',
+    version='1.0',
     description="Addon for ElasticSearch integration with Plone",
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
