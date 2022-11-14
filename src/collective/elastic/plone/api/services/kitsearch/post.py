85461d276fedbd7a7290a8b7605cc47a924961a6
Katja Süss <k.suess@rohberg.ch>
2022-11-14 22:06:24 +0100

    Do request elasticsearch

--- src/collective/elastic/plone/api/services/kitsearch/post.py
+++ src/collective/elastic/plone/api/services/kitsearch/post.py
@@ -1,8 +1,19 @@
-from plone.restapi.interfaces import IExpandableElement
 from plone.restapi.deserializer import json_body
 from plone.restapi.services import Service
 
 import json
+import requests
+
+
+def search(data):
+    elasticsearch_url = data.get("elasticsearch_url", "http://localhost:9200")
+    elasticsearch_index = data.get("elasticsearch_index", "plone")
+    resp = requests.post(
+        f"{elasticsearch_url}/{elasticsearch_index}/_search",
+        headers={"Accept": "application/json", "Content-Type": "application/json"},
+        json=data.get("elasticsearch_payload", {}),
+    )
+    return json.loads(resp.text)
 
 
 class KitsearchGet(Service):
@@ -12,11 +23,13 @@ class KitsearchGet(Service):
         if not data:
             return {}
 
-        # TODO Get items from elasticsearch. This is an example response:
-        from collective.elastic.plone.api.services.kitsearch.response_example import (
-            elasticsearchresponse_example,
-        )
+        # # An example response:
+        # from collective.elastic.plone.api.services.kitsearch.response_example import (
+        #     elasticsearchresponse_example,
+        # )
 
-        elasticsearchresponse = json.loads(elasticsearchresponse_example)
+        # elasticsearchresponse = json.loads(elasticsearchresponse_example)
+
+        elasticsearchresponse = search(data)
 
         return elasticsearchresponse
