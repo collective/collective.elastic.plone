e3412007c1c24a86aba75ac84abce90bcfa7df42
Katja Süss <k.suess@rohberg.ch>
2023-03-03 12:59:25 +0100

    Prepare release

--- CHANGES.rst
+++ CHANGES.rst
@@ -5,7 +5,7 @@ Changelog
 1.1 (unreleased)
 ----------------
 
-- Search REST API service for ElasticSearch querying with Plone security check. #6 @ksuess
+- Search REST API service @kitsearch for ElasticSearch querying with Plone security check. #6 @ksuess
 
 
 1.0 (2022-11-08)
--- setup.py
+++ setup.py
@@ -28,6 +28,7 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "Operating System :: OS Independent",
         "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
     ],
@@ -47,7 +48,7 @@ setup(
     package_dir={'': 'src'},
     include_package_data=True,
     zip_safe=False,
-    python_requires=">=3.6",
+    python_requires=">=3.7",
     install_requires=[
         'collective.elastic.ingest',
         'jinja2',
