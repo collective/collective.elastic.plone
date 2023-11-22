570abc524cd0a9c93e5b122ee2beb46aa8856b89
Jens W. Klein <jk@kleinundpartner.at>
2023-11-22 17:52:11 +0100

    relative imports are faster

--- src/collective/elastic/plone/restapi/schema.py
+++ src/collective/elastic/plone/restapi/schema.py
@@ -1,4 +1,4 @@
-from collective.elastic.plone.restapi.service import Service
+from .service import Service
 from plone.behavior.interfaces import IBehavior
 from Products.CMFCore.interfaces import ITypeInformation
 from zope.component import getAllUtilitiesRegisteredFor
