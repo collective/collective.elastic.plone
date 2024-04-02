bdcce43484c748cb2da7a44b007a828ec57a10de
Katja Süss <k.suess@rohberg.ch>
2024-04-02 13:15:35 +0200

    Add @@clear-and-update-index-server-index

--- src/collective/elastic/plone/browser/update.py
+++ src/collective/elastic/plone/browser/update.py
@@ -1,5 +1,5 @@
 from .. import INDEX_NAME
-from collective.elastic.ingest.celery import index
+from collective.elastic.ingest.celery import deleteindex, index
 from plone import api
 from Products.Five.browser import BrowserView
 
@@ -15,3 +15,20 @@ class UpdateElastisearch(BrowserView):
             index.delay(path, 0, INDEX_NAME)
             count += 1
         return f"queued {count}"
+
+
+class ClearAndUpdateElastisearch(BrowserView):
+    def __call__(self):
+        # Delete index
+        deleteindex.delay(INDEX_NAME)
+
+        # Index content
+        cat = api.portal.get_tool("portal_catalog")
+        count = 0
+        for path in cat._catalog.uids:
+            if path.endswith("/portal_catalog"):
+                # no idea why it is in the list, ignore
+                continue
+            index.delay(path, 0, INDEX_NAME)
+            count += 1
+        return f"Index '{INDEX_NAME}' rebuild."
